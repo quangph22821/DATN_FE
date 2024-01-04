ed41ae6e7e4f7a221de99d3e0065aa244d986549
luontd <110533951+luontd@users.noreply.github.com>
2024-01-04 23:40:59 +0700

    giao dien detail

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import HomePage from "./pages/home";
 import UserLayout from "./components/layout/UserLayout";
 import Signup from "./pages/signup/signup";
 import AboutPage from "./pages/about";
+import DetailPage from "./pages/detail";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <UserLayout />,
     children: [
       { path: "/", element: <HomePage /> },
+      {path:'detail/:id', element:<DetailPage/>},
       {path:"/signup",element:<Signup/>},
       {path:"/about",element:<AboutPage/>}
     ],
--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,12 +1,25 @@
 
 const DetailPage = () => {
- 
-
-
   return (
-   <>
-   <h1>Deatil</h1>
-   </>
+    <>
+      {/* Breadcrumb Start */}
+      <div className="container-fluid">
+            <div className="row px-xl-5">
+              <div className="col-12">
+                <nav className="breadcrumb bg-light mb-30">
+                  <a className="breadcrumb-item text-dark" href="#">
+                    Trang chủ
+                  </a>
+                  <a className="breadcrumb-item text-dark" href="#">
+                    Cửa hàng
+                  </a>
+                  <span className="breadcrumb-item active">Chi tết sản phẩm</span>
+                </nav>
+              </div>
+            </div>
+          </div>
+          {/* Breadcrumb End */}
+    </>
   )
 };
 
