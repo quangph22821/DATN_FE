21f8826aad8bc3312c3abf8c879334696fa19a16
luontd <110533951+luontd@users.noreply.github.com>
2024-01-05 13:31:55 +0700

    giao diện shop

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -4,8 +4,272 @@ const ShopPage = () => {
 
   return (
     <>
-        
-      
+      {/* Breadcrumb Start */}
+      <div className="container-fluid">
+        <div className="row px-xl-5">
+          <div className="col-12">
+            <nav className="breadcrumb bg-light mb-30">
+              <a className="breadcrumb-item text-dark" href="#">
+                Trang chủ
+              </a>
+              <a className="breadcrumb-item text-dark" href="#">
+                Của hàng
+              </a>
+              <span className="breadcrumb-item active">Danh sách sản phẩm</span>
+            </nav>
+          </div>
+        </div>
+      </div>
+      {/* Breadcrumb End */}
+
+      {/* Shop Start */}
+      <div className="container-fluid">
+        <div className="row px-xl-5">
+          {/* Shop Sidebar Start */}
+          <div className="col-lg-3 col-md-4">
+            {/* Price Start */}
+            <h5 className="section-title position-relative text-uppercase mb-3">
+              <span className="bg-secondary pr-3">Lọc theo giá</span>
+            </h5>
+            <div className="bg-light p-4 mb-30">
+              <form>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="price-all"
+                  />
+                  <label className="custom-control-label" htmlFor="price-all">
+                    Tất cả giá cả
+                  </label>
+                  <span className="badge border font-weight-normal">1000</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="price-all"
+                  />
+                  <label className="custom-control-label" htmlFor="price-1">
+                    $0 - $100
+                  </label>
+                  <span className="badge border font-weight-normal">150</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="price-2"
+                  />
+                  <label className="custom-control-label" htmlFor="price-2">
+                    $100 - $200
+                  </label>
+                  <span className="badge border font-weight-normal">295</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="price-3"
+                  />
+                  <label className="custom-control-label" htmlFor="price-3">
+                    $200 - $300
+                  </label>
+                  <span className="badge border font-weight-normal">246</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="price-4"
+                  />
+                  <label className="custom-control-label" htmlFor="price-4">
+                    $300 - $400
+                  </label>
+                  <span className="badge border font-weight-normal">145</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="price-5"
+                  />
+                  <label className="custom-control-label" htmlFor="price-5">
+                    $400 - $500
+                  </label>
+                  <span className="badge border font-weight-normal">168</span>
+                </div>
+              </form>
+            </div>
+            {/* Price End */}
+
+            {/* Size Start */}
+            <h5 className="section-title position-relative text-uppercase mb-3">
+              <span className="bg-secondary pr-3">Lọc theo chất liệu</span>
+            </h5>
+            <div className="bg-light p-4 mb-30">
+              <form>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="size-all"
+                  />
+                  <label className="custom-control-label" htmlFor="size-all">
+                    Tất cả chất liệu
+                  </label>
+                  <span className="badge border font-weight-normal">1000</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="size-1"
+                  />
+                  <label className="custom-control-label" htmlFor="size-1">
+                    XS
+                  </label>
+                  <span className="badge border font-weight-normal">150</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="size-2"
+                  />
+                  <label className="custom-control-label" htmlFor="size-2">
+                    S
+                  </label>
+                  <span className="badge border font-weight-normal">295</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="size-3"
+                  />
+                  <label className="custom-control-label" htmlFor="size-3">
+                    M
+                  </label>
+                  <span className="badge border font-weight-normal">246</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="size-4"
+                  />
+                  <label className="custom-control-label" htmlFor="size-4">
+                    L
+                  </label>
+                  <span className="badge border font-weight-normal">145</span>
+                </div>
+                <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="size-5"
+                  />
+                  <label className="custom-control-label" htmlFor="size-5">
+                    XL
+                  </label>
+                  <span className="badge border font-weight-normal">168</span>
+                </div>
+              </form>
+            </div>
+            {/* Size End */}
+          </div>
+          {/* Shop Sidebar End */}
+
+          {/* Shop Product Start */}
+          <div className="col-lg-9 col-md-8">
+            <div className="row pb-3">
+              {/* {product.map((item) => ( */}
+                <div className="col-lg-4 col-md-6 col-sm-6 pb-1">
+                  <div className="product-item bg-light mb-4">
+                    <div className="product-img position-relative overflow-hidden">
+                      <img
+                        className="img-fluid w-100"
+                        // src={item.img?.[0]}
+                        alt=""
+                      />
+                      <div className="product-action">
+                        <a className="btn btn-outline-dark btn-square" href="">
+                          <i className="fa fa-shopping-cart" />
+                        </a>
+                        <a className="btn btn-outline-dark btn-square" href="">
+                          <i className="far fa-heart" />
+                        </a>
+                        {/* <Link
+                          className="btn btn-outline-dark btn-square"
+                          to={`/detail/${item._id}`}
+                        >
+                          <i className="fa fa-search" />
+                        </Link> */}
+                      </div>
+                    </div>
+                    <div className="text-center py-4">
+                      {/* <Link
+                        className="h6 text-decoration-none text-truncate"
+                        to={`/detail/${item._id}`}
+                      >
+                        {item.name}
+                      </Link> */}
+                      <div className="d-flex align-items-center justify-content-center mt-2">
+                        {/* <h5>{item.price}.000 VNĐ</h5> */}
+                        <h6 className="text-muted ml-2">
+                          {/* <del>$123.00</del> */}
+                        </h6>
+                      </div>
+                      <div className="d-flex align-items-center justify-content-center mb-1">
+                        <small className="fa fa-star text-primary mr-1" />
+                        <small className="fa fa-star text-primary mr-1" />
+                        <small className="fa fa-star text-primary mr-1" />
+                        <small className="fa fa-star text-primary mr-1" />
+                        <small className="fa fa-star text-primary mr-1" />
+                        <small>(99)</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              {/* ))} */}
+              <div className="col-12">
+                <nav>
+                  <ul className="pagination justify-content-center">
+                    <li className="page-item disabled">
+                      <a className="page-link" href="#">
+                        Previous
+                      </a>
+                    </li>
+                    <li className="page-item active">
+                      <a className="page-link" href="#">
+                        1
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        2
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        3
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        Next
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          {/* Shop Product End */}
+        </div>
+      </div>
+      {/* Shop End */}
     </>
    
   );
