ab621941777ca1d133b30476da805ecd14df3390
Quang Hau <quanghau457@gmail.com>
2024-01-09 22:53:19 +0700

    contact2

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -81,6 +81,36 @@ const ContactPage = () => {
                 </button>
               </form>
 
+              </div>
+          </div>
+          <div className="col-lg-5 mb-5">
+            <div className="bg-light p-30 mb-30">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8354772940997!2d105.72923707389414!3d21.0392679806127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31345550b525aa03%3A0x3fdefc40f69a023a!2zQ2FvIMSR4bqzbmcgRlBUIFBo4buRIFRy4buLbmggVsSDbiBCw7QgLCBQaMaw4budbmcgUGjGsMahbmcgQ2FuaCAsIHF14bqtbiBU4burIExpw6pt!5e0!3m2!1sen!2s!4v1701149443406!5m2!1sen!2s"
+                style={{ width: "100%", height: 250 }}
+                frameBorder={0}
+                aria-hidden="false"
+                tabIndex={0}
+              />
+            </div>
+            <div className="bg-light p-30 mb-3">
+              <p className="mb-2">
+                <i className="fa fa-map-marker-alt text-primary mr-3" />
+                123 Street, New York, USA
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-envelope text-primary mr-3" />
+                info@example.com
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-phone-alt text-primary mr-3" />
+                +012 345 67890
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
                 </>
   
 };
