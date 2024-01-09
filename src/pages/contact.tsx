2b555a271c5f42a7a7b0236c1a58ed24c730d61b
Quang Hau <quanghau457@gmail.com>
2024-01-09 22:53:59 +0700

    contacthoanthien

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,8 @@
 const ContactPage = () => {
-  return <>
-   {/* Breadcrumb Start */}
-   <div className="container-fluid">
+  return (
+    <>
+      {/* Breadcrumb Start */}
+      <div className="container-fluid">
         <div className="row px-xl-5">
           <div className="col-12">
             <nav className="breadcrumb bg-light mb-30">
@@ -41,7 +42,8 @@ const ContactPage = () => {
                   />
                   <p className="help-block text-danger" />
                 </div>
-                
+
+
                 <div className="control-group">
                   <input
                     type="email"
@@ -81,7 +83,70 @@ const ContactPage = () => {
                 </button>
               </form>
 
-              </div>
+
+
+
+              {/* dsadasdasdsa */}
+              {/* <form action="https://formspree.io/f/mkndnapk" method="POST" name="sentMessage" id="contactForm">
+                <div className="control-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="Your Name"
+                    required
+                    data-validation-required-message="Please enter your name"
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+                <div className="control-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Your Email"
+                    required
+                    data-validation-required-message="Please enter your email"
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+                <div className="control-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="subject"
+                    placeholder="Subject"
+                    required
+                    data-validation-required-message="Please enter a subject"
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+                <div className="control-group">
+                  <textarea
+                    className="form-control"
+                    rows={8}
+                    id="message"
+                    placeholder="Message"
+                    required
+                    data-validation-required-message="Please enter your message"
+                    defaultValue={""}
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+                <div>
+                  <button
+                    className="btn btn-primary py-2 px-4"
+                    type="submit"
+                    id="sendMessageButton"
+                  >
+                    Send Message
+                  </button>
+                </div>
+              </form> */}
+
+
+
+            </div>
           </div>
           <div className="col-lg-5 mb-5">
             <div className="bg-light p-30 mb-30">
@@ -110,9 +175,9 @@ const ContactPage = () => {
           </div>
         </div>
       </div>
-      
-                </>
-  
+      {/* Contact End */}
+    </>
+  );
 };
 
 export default ContactPage;
