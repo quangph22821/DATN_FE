3de74a509428ef0e16d91728f36b66b922c999e7
Quang Hau <quanghau457@gmail.com>
2024-01-09 22:52:04 +0700

    contact1

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,5 @@
 const ContactPage = () => {
-  return (
-   <>
+  return <>
    {/* Breadcrumb Start */}
    <div className="container-fluid">
         <div className="row px-xl-5">
@@ -43,8 +42,47 @@ const ContactPage = () => {
                   <p className="help-block text-danger" />
                 </div>
                 
+                <div className="control-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="Your Email"
+                    required
+                    data-validation-required-message="Please enter your email"
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+
+                
+                <div className="control-group">
+                  <textarea
+                    className="form-control"
+                    rows={8}
+                    id="message"
+                    name="message"
+                    placeholder="Message"
+                    required
+                    data-validation-required-message="Please enter your message"
+                    defaultValue={""}
+                  />
+                  <p className="help-block text-danger" />
+                </div>
+
+
+
+                <button
+                  className="btn btn-primary py-2 px-4"
+                  type="submit"
+                  id="sendMessageButton"
+                >
+                  Send Message
+                </button>
+              </form>
+
                 </>
-  );
+  
 };
 
 export default ContactPage;
