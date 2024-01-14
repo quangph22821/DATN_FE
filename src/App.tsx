a881ed794eb97078d071b012494ed2cb2fcfdee7
DangQuyen1923 <dangxuanquyen1923@gamil.com>
2024-01-14 23:55:32 +0700

    list product

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import UpdateOrigin from "./admin/origin/update";
 import ListMaterialPage from "./admin/material/listMaterial";
 import CreateMaterial from "./admin/material/create";
 import UpdateMaterial from "./admin/material/update";
+import ListProductsPage from "./admin/products/listProduct";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -50,7 +51,7 @@ const router = createBrowserRouter([
       { path: "/admin", element: <DashboardPage /> },
 
       // Products
-      // { path: "/admin/listPro", element: <ListProductsPage /> },
+      { path: "/admin/listPro", element: <ListProductsPage /> },
       // { path: "/admin/createPro", element: <CreateProducts /> },
       // { path: "/admin/updatePro/:id", element: <UpdateProducts /> },
 
